89c4d8feaf70105b1d53548d668d3699d9baf54d
BrayanMen <brayanjmr880@gmail.com>
2025-07-20 20:43:36 -0300

    model,services,controller products

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { getAllProducts, getProductById } from '../controllers/products.controller.js';
+import { createProduct, getAllProducts, getProductById, searchProductsByName } from '../controllers/products.controller.js';
+
 const router = Router();
 
 router.get('/', getAllProducts);
+router.get('/search', searchProductsByName)
+router.post('/create', createProduct)
 router.get('/:id', getProductById);
+// router.put('/:id', );
+// router.delete('/:id', );
 
 export default router;
