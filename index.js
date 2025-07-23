e1b53aa4fec4493ff98d616ad65ab3ed895e6233
BrayanMen <brayanjmr880@gmail.com>
2025-07-23 02:50:32 -0300

    config vercel

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import configEnv from './src/config/env.config.js';
 import server from './src/server.js';
 
 const PORT = configEnv.port;
-const ENV = configEnv.env;
 const ENV_URL = configEnv.workEnviroment;
 server.listen(PORT, () => {
     console.log(`
-        Servidor funcionando exitosamente en puerto: ${PORT};
-        Servidor en entorno: ${ENV};
+        Servidor funcionando exitosamente en puerto: ${PORT};       
         Url del servidor: ${ENV_URL};
         `);
 });
