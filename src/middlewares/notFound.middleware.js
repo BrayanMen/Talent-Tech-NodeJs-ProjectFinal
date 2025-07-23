a05a8e5c3d976feb8c6b59dbb53157d85e3fbf1b
BrayanMen <brayanjmr880@gmail.com>
2025-07-23 03:01:21 -0300

    config vercel

--- src/middlewares/notFound.middleware.js
+++ src/middlewares/notFound.middleware.js
@@ -3,24 +3,22 @@ export const notFound = (req, res, next) => {
         error: 'Ruta no encontrada',
         message: `La ruta ${req.originalUrl} no existe en este servidor`,
         routes: [
-            `  
-              GET /api/products → Obtener todos los productos.
-              GET /api/products/:id → Obtener producto por ID.
-              GET /api/products/search?name=xxx → Buscar producto por nombre.
-              POST /api/products/create → Crear producto (requiere token de admin).
-              PUT /api/products/:id → Modificar producto (requiere token de admin).
-              DELETE /api/products/:id
+            'GET /api/products → Obtener todos los productos.',
+            'GET /api/products/:id → Obtener producto por ID.',
+            'GET /api/products/search?name=xxx → Buscar producto por nombre.',
+            'POST /api/products/create → Crear producto (requiere token de admin).',
+            'PUT /api/products/:id → Modificar producto (requiere token de admin).',
+            'DELETE /api/products/:id → Eliminar producto (requiere token de admin).',
 
-              POST /auth/login → Devuelve token si credenciales válidas.
-              POST /auth/register → Registra un nuevo usuario.
+            'POST /auth/login → Devuelve token si credenciales válidas.',
+            'POST /auth/register → Registra un nuevo usuario.',
 
-              GET /api/users → Listar todos los usuarios (admin).
-              GET /api/users/profile → Perfil del usuario autenticado.
-              GET /api/users/profile/update → Actualizar perfil del usuario.
-              DELETE /api/users/profile → Eliminar cuenta del usuario.
-              PATCH /api/users/change-password → Cambiar contraseña.
-              PATCH /api/users/wishlist → Agregar/quitar producto de favoritos.
-            `,
+            'GET /api/users → Listar todos los usuarios (admin).',
+            'GET /api/users/profile → Perfil del usuario autenticado.',
+            'GET /api/users/profile/update → Actualizar perfil del usuario.',
+            'DELETE /api/users/profile → Eliminar cuenta del usuario.',
+            'PATCH /api/users/change-password → Cambiar contraseña.',
+            'PATCH /api/users/wishlist → Agregar/quitar producto de favoritos.',
         ],
     });
 };
